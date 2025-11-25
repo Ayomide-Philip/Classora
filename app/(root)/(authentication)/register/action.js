a1b37aa77877dbe07b61938af782ce7b927ba14a
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-11-25 21:35:37 +0100

    Refactor registration validation to improve email and name checks

--- app/(root)/(authentication)/register/action.js
+++ app/(root)/(authentication)/register/action.js
@@ -1,6 +1,7 @@
 import { toast } from "react-toastify";
 
 export default async function RegisterAction(formData) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const name = formData.get("fullname");
   const email = formData.get("email");
   const password = formData.get("password");
@@ -12,9 +13,13 @@ export default async function RegisterAction(formData) {
   if (!password) return toast.error("Password is required");
   if (!confirmPassword) return toast.error("Password is required");
 
+  if (name.length < 5)
+    return toast.error("Name should be at least 5 characters");
+
   if (!name.trim().includes(" "))
     return toast.error("Your FullName is required");
 
-  if (name.length < 5)
-    return toast.error("Name should be at least 5 characters");
+  if (!email || email.trim()) return toast.error("Email address is required");
+
+  if (!emailRegex.test(email)) return toast.error("Invalid email address");
 }
