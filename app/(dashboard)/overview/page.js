85063c88515683ecd982242df6061ada47a49cb4
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-11-29 22:11:03 +0100

    feat: Implement user fetching in OverviewPage to display ActiveBoard or EmptyBoard

--- app/(dashboard)/overview/page.js
+++ app/(dashboard)/overview/page.js
@@ -1,9 +1,19 @@
+import ActiveBoard from "@/components/dashboard/overview/activeBoard";
 import EmptyBoard from "@/components/dashboard/overview/emptyBoard";
+import { BASE_URL } from "@/libs/config";
+import { cookies } from "next/headers";
 
-export default function OverviewPage() {
+export default async function OverviewPage() {
+  const userRequest = await fetch(`${BASE_URL}/api/users`, {
+    method: "GET",
+    headers: {
+      Cookie: (await cookies()).toString(),
+      "Content-Type": "application/json",
+    },
+  });
+  const { user } = await userRequest.json();
+  console.log(user);
   return (
-    <>
-      <EmptyBoard />
-    </>
+    <>{user?.board ? <ActiveBoard board={user?.board} /> : <EmptyBoard />}</>
   );
 }
