7a009e8f2a25d77862383fbe913b31f808b0e738
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-05 00:54:50 +0100

    feat: Add board existence check in GET endpoint for courses retrieval

--- app/api/boards/[id]/courses/route.js
+++ app/api/boards/[id]/courses/route.js
@@ -1,11 +1,22 @@
 import { NextResponse } from "next/server";
 import { connectDatabase } from "@/libs/connectDatabase";
 import Courses from "@/libs/models/courses.models";
+import Boards from "@/libs/models/boards.models";
 
 export async function GET(req, { params }) {
   const { id } = await params;
   try {
     await connectDatabase();
+    const board = await Boards.findOne({ _id: id });
+    if (!board) {
+      return NextResponse.json(
+        { error: "Board doesn't exist" },
+        {
+          status: 404,
+        }
+      );
+    }
+    // check for all course where boardId = id
     const course = await Courses.find({ boardId: id });
 
     return NextResponse.json(
@@ -23,4 +34,13 @@ export async function GET(req, { params }) {
       }
     );
   }
+}
+
+export async function POST(req, { params }) {
+  return NextResponse.json(
+    { message: "POST new course" },
+    {
+      status: 200,
+    }
+  );
 }
