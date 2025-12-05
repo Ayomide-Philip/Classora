ea007af0162203c60fdf83aba9c75b0965b0f0fd
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-05 22:11:28 +0100

    feat: Refactor POST request to include validation for course title and code

--- app/api/boards/[id]/courses/route.js
+++ app/api/boards/[id]/courses/route.js
@@ -40,7 +40,6 @@ export async function POST(req, { params }) {
   const { id } = await params;
   const data = await req.json();
   const {
-    boardId,
     userId,
     courseTitle,
     courseCode,
@@ -51,6 +50,56 @@ export async function POST(req, { params }) {
     semester,
   } = data;
   console.log(data);
+
+  //  checking if the board exists
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board doesn't exist" },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // checking if the user id is passed
+  if (!userId) {
+    return NextResponse.json(
+      { error: "User doesn't exist" },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // validating the course title
+  if (!courseTitle || !courseTitle.trim()) {
+    return NextResponse.json(
+      { error: "Course title is required" },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  if (courseTitle.trim().length < 5) {
+    return NextResponse.json(
+      { error: "Course title should be at least 5 characters" },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // validating the course code
+  if (!courseCode || !courseCode.trim()) {
+    return NextResponse.json(
+      { error: "Course Code is required" },
+      {
+        status: 400,
+      }
+    );
+  }
+
   return NextResponse.json(
     { boardId: id },
     {
