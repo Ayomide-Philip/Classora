3a224495736ab47c770a5ac79413f2c07858a2a7
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-10 14:52:03 +0100

    feat: implement GET route for fetching class details with error handling

---
+++ app/api/boards/[id]/classes/[classId]/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { connectDatabase } from "@/libs/connectDatabase";
+import Classes from "@/libs/models/classes.models";
+import Courses from "@/libs/models/courses.models";
+
+export async function GET(req, { params }) {
+  const { id, classId } = await params;
+  try {
+    await connectDatabase();
+    // check if the class exist
+    const classes = await Classes.findById(classId).populate(
+      "courseId",
+      "courseTitle courseCode courseDescription semester courseUnit"
+    );
+    if (!classes) {
+      return NextResponse.json(
+        { error: "Class does not exist" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { classes },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "An error occurred while getting class" },
+      {
+        status: 400,
+      }
+    );
+  }
+}
