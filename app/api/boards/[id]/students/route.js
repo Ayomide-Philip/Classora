6879d6da6837b8f274cfae718c73b0dde3597881
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-13 00:48:12 +0100

    feat: implement student management page with static data and search functionality

---
+++ app/api/boards/[id]/students/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  const { id } = await params;
+  if(!id){
+      return NextResponse.json({error:"Board id doesn't exist"},{
+          status: 404
+      })
+  }
+  try {
+return NextResponse.json(
+    { message: "Getting all students", boardId: id },
+    {
+      status: 200,
+    }
+  );
+  }catch (err){
+      console.log(err);
+      return NextResponse.json({error:"An error occurred while getting all students"},{
+          status:400
+      });
+  }
+}
