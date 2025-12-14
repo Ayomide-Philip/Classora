f978244f14989b7d98b2c7eeb6ed2746c390ed41
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-14 23:24:10 +0100

    fix: add validation for board and student IDs in PUT request

--- app/api/boards/[id]/students/[studentId]/admin/route.js
+++ app/api/boards/[id]/students/[studentId]/admin/route.js
@@ -1,6 +1,26 @@
 import { NextResponse } from "next/server";
 
 export async function PUT(req, { params }) {
+    const { id, studentId } = await params;
+
+      // checking board id
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board Id not defined" },
+      {
+        status: 404,
+      }
+    );
+  }
+  // checking studentId
+  if (!studentId) {
+    return NextResponse.json(
+      { error: "Student Id not defined" },
+      {
+        status: 404,
+      }
+    );
+  }
   return NextResponse.json(
     { message: "PUT user role" },
     {
