ab89cf77c8554f8b142a89b6d9147c31521e31e7
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-14 22:49:56 +0100

    updating my route.js

--- app/api/boards/[id]/students/[studentId]/route.js
+++ app/api/boards/[id]/students/[studentId]/route.js
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
   const { id, studentId } = await params;
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board Id not defined" },
+      {
+        status: 404,
+      }
+    );
+  }
   return NextResponse.json(
     { boardId: id, studentId },
     {
