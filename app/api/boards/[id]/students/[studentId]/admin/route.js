01665bde8a23f5a5a2e3185600294d77f4daae6f
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-14 23:31:19 +0100

    refactor: streamline PUT request handling and improve error responses

--- app/api/boards/[id]/students/[studentId]/admin/route.js
+++ app/api/boards/[id]/students/[studentId]/admin/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
+import { connectDatabase } from "@/libs/connectDatabase";
+import Boards from "@/libs/models/boards.models";
 
 export async function PUT(req, { params }) {
-    const { id, studentId } = await params;
+  const { id, studentId } = await params;
 
-      // checking board id
+  // checking board id
   if (!id) {
     return NextResponse.json(
       { error: "Board Id not defined" },
@@ -21,10 +23,37 @@ export async function PUT(req, { params }) {
       }
     );
   }
-  return NextResponse.json(
-    { message: "PUT user role" },
-    {
-      status: 200,
+
+  try {
+    await connectDatabase();
+    // check if the board exists
+    const boards = await Boards.findById(id).populate("students", "-password");
+    // if board does not exist return error
+    if (!boards) {
+      return NextResponse.json(
+        { error: "Board not found" },
+        {
+          status: 404,
+        }
+      );
     }
-  );
+    // check if the student exist
+    const studentExist = boards?.students.map((student) => {
+      return student._id.toString() === studentId.toString();
+    });
+    return NextResponse.json(
+      { message: "PUT user role" },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "An error occurred while trying to update student role" },
+      {
+        status: 400,
+      }
+    );
+  }
 }
