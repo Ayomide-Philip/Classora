702ff2184b8bb066adb0ff42d482f02638b80ca6
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-16 10:32:56 +0100

    fix: remove console log for students in page component

--- app/(dashboard)/students/page.js
+++ app/(dashboard)/students/page.js
@@ -22,8 +22,7 @@ export default async function Page() {
     return redirect("/overview");
   }
   const { students } = response;
-  
-  console.log(students);
+
   return (
     <main className="px-4 py-6 sm:py-8 md:px-8">
       <div className="mx-auto max-w-6xl">
