3ddba50233ab89359621801964769273558792cb
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-20 00:39:22 +0100

    refactor: add console log for user ID in GET function for debugging purposes

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
+    const {id} = await params;
+    console.log(id)
   return NextResponse.json(
     { message: "GET users by ID" },
     {
