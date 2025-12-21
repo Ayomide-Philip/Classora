894136930c5664b82851d8c1990f95d4bcc42756
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-22 00:06:04 +0100

    add assignment schema with boardId and courseId fields for MongoDB

--- libs/models/assignments.models.js
+++ libs/models/assignments.models.js
@@ -1,7 +1,18 @@
 import { models, model, Schema } from "mongoose";
 
 const assignmentSchema = new Schema(
-  {},
+  {
+    boardId: {
+      type: Schema.Types.ObjectId,
+      ref: "Boards",
+      required: true,
+    },
+    courseId: {
+      type: Schema.Types.ObjectId,
+      ref: "Courses",
+      required: true,
+    },
+  },
   {
     timestamps: true,
   }
