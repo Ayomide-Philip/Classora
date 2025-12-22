6d1cee31bbedcd0ffe85c488befebcae8b0283ff
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-22 01:44:58 +0100

    fix: validate board ID and handle not found errors in GET assignments endpoint

--- app/api/boards/[id]/assignments/route.js
+++ app/api/boards/[id]/assignments/route.js
@@ -1,10 +1,28 @@
 import { NextResponse } from "next/server";
 import { connectDatabase } from "@/libs/connectDatabase";
+import Boards from "@/libs/models/boards.models";
 
 export async function GET(req, { params }) {
   const { id } = await params;
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board Id is not defined" },
+      {
+        status: 404,
+      }
+    );
+  }
   try {
     await connectDatabase();
+    const board = await Boards.findById(id);
+    if (!board) {
+      return NextResponse.json(
+        { error: "Board not found." },
+        {
+          status: 404,
+        }
+      );
+    }
     return NextResponse.json(
       { error: "GET all assignments" },
       {
