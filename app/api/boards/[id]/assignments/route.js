da70b6ce0cce4acc5d76f3204fd342cf08ebec6b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-22 01:29:08 +0100

    fix: handle errors in GET assignments endpoint

--- app/api/boards/[id]/assignments/route.js
+++ app/api/boards/[id]/assignments/route.js
@@ -1,10 +1,21 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-  return NextResponse.json(
-    { error: "GET all assignments" },
-    {
-      status: 200,
-    }
-  );
+  const { id } = await params;
+  try {
+    return NextResponse.json(
+      { error: "GET all assignments" },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "An error was encountered while trying to get all assignments" },
+      {
+        status: 500,
+      }
+    );
+  }
 }
