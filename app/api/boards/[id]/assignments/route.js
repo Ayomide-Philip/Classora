5c67a223277daeff31d1c8574f418650094508ee
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-24 22:24:19 +0100

    updating my route.js

--- app/api/boards/[id]/assignments/route.js
+++ app/api/boards/[id]/assignments/route.js
@@ -46,6 +46,96 @@ export async function GET(req, { params }) {
 }
 
 export async function POST(req, { params }) {
+  const { id } = await params;
+  const {
+    courseId,
+    userId,
+    title,
+    description,
+    dueDate,
+    submitMode,
+    googleFormUrl,
+  } = await req.json();
+  // if board Id doesnt exist
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board Id is not defined" },
+      {
+        status: 404,
+      }
+    );
+  }
+  // if no course id
+  if (!courseId) {
+    return NextResponse.json(
+      { error: "Assignment doesnt have any reference course" },
+      {
+        status: 404,
+      }
+    );
+  }
+  // if title does not exist
+  if (!title || !title.trim()) {
+    return NextResponse.json(
+      { error: "Assignments title is required" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // if title is less than 5 character
+  if (title.trim().length < 5) {
+    return NextResponse.json(
+      { error: "Assignment title cant be less than 5 characters" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // if description doesnt exist
+  if (!description || !description.trim()) {
+    return NextResponse.json(
+      { error: "Assignment description is required" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // if description is less than 10 characters
+  if (description.trim().length < 10) {
+    return NextResponse.json(
+      { error: "Assignments description can not be less than 10 characters" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // check submit mode
+  if (!submitMode || !submitMode.trim()) {
+    return NextResponse.json(
+      { error: "Assignment submit mode should be stated" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // check for which inputted submitMode is not in the enum
+  if (
+    submitMode.trim() !== "googleForm" &&
+    submitMode.trim() !== "directSubmission" &&
+    submitMode.trim() !== "unknown"
+  ) {
+    return NextResponse.json(
+      {
+        error:
+          "Assignment submit mode can either be Google form, Direct Submission or unknown",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
   return NextResponse.json(
     { message: "Post a new assignment" },
     {
