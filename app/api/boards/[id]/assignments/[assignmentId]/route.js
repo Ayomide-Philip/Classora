257fc1c9b5606109d2bf010f5882ed8f2d79c5ed
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-26 00:18:02 +0100

    feat: implement DELETE route for assignments with user authorization and error handling

--- app/api/boards/[id]/assignments/[assignmentId]/route.js
+++ app/api/boards/[id]/assignments/[assignmentId]/route.js
@@ -73,4 +73,99 @@ export async function GET(req, { params }) {
   }
 }
 
-export async function DELETE(req, { params }) {}
+export async function DELETE(req, { params }) {
+  const { userId } = await req.json();
+  const { id, assignmentId } = await params;
+  // if board id doesnt exist
+  if (!id) {
+    return NextResponse.json(
+      { error: "Board Id not found" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // if assignment id doesnt exist
+  if (!assignmentId) {
+    return NextResponse.json(
+      { error: "Assignments Id not found" },
+      {
+        status: 400,
+      }
+    );
+  }
+  try {
+    await connectDatabase();
+    // get the user board info
+    const user = await Users.findById(userId).select("board");
+    // if user doesnt have admin prviledges
+    if (user?.board?.role !== "admin" && user?.board?.role !== "owner") {
+      return NextResponse.json(
+        { error: "You dont have access to delete and assignments" },
+        {
+          status: 400,
+        }
+      );
+    }
+    // check if the board exists
+    const board = await Boards.findById(id);
+    if (!board) {
+      return NextResponse.json(
+        { error: "Board not found" },
+        {
+          status: 400,
+        }
+      );
+    }
+    // if assignments doesnt exist
+    const assignments = await Assignments.findOne({
+      _id: assignmentId,
+      boardId: id,
+    });
+    if (!assignments) {
+      return NextResponse.json(
+        { error: "Assignment not found" },
+        {
+          status: 400,
+        }
+      );
+    }
+    // check the course
+    const course = await Courses.findById(assignments?.courseId);
+    const remainingAssignments = course?.assignments.filter((assignment) => {
+      return assignment.toString() !== assignmentId;
+    });
+    course.assignments = remainingAssignments ? remainingAssignments : [];
+    await course.save();
+    // delete the course also from the assignments page
+    const deleteAssignments = await Assignments.deleteOne({
+      _id: assignmentId,
+      boardId: id,
+    });
+    if (
+      !deleteAssignments.acknowledged &&
+      deleteAssignments.deletedCount !== 1
+    ) {
+      return NextResponse.json(
+        { error: "Unable to delete assignments" },
+        {
+          status: 400,
+        }
+      );
+    }
+    return NextResponse.json(
+      { message: "Successfully deleted Assignments" },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Unable to delete assignment" },
+      {
+        status: 400,
+      }
+    );
+  }
+}
